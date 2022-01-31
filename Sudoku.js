67657547419a29da92660878a0d42c500e085318
bobesena <phbschneider@hotmail.com>
2022-01-30 22:44:46 -0300

    sudoku Inicial

---
+++ Sudoku.js
@@ -0,0 +1,16 @@
+function getLinha(linha){
+    return console.log("A linha eh "+ linha);
+  }
+  function getColuna(coluna){
+    return console.log("A coluna eh " + coluna);
+  }
+  function getBloco(bloco){
+    
+    return console.log("O bloco eh " + bloco);
+  }
+  function main(){
+    getLinha(3);
+    getColuna(4);
+    getBloco(5);
+  }
+  main();
